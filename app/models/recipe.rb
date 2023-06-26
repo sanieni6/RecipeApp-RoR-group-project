631841611eebd258c462916020950cffc0260510
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-26 16:03:48 -0500

    Fix linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
-    
-        belongs_to :user, foreign_key: :user_id
-        has_many :recipe_foods, foreign_key: :recipe_id
-    
-        validates :name, presence: true
+  belongs_to :user, foreign_key: :user_id
+  has_many :recipe_foods, foreign_key: :recipe_id
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :foods, foreign_key: :user_id
-    has_many :recipes, foreign_key: :user_id
+  has_many :foods, foreign_key: :user_id
+  has_many :recipes, foreign_key: :user_id
 
-    validates :name, presence: true
-    
+  validates :name, presence: true
 end
