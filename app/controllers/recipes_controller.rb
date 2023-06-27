ddb7666077326aea9ca4c94b36e82cf886ffa839
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-27 15:47:42 -0500

    Create the recipes views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,33 @@
 class RecipesController < ApplicationController
+  load_resource through: :current_user
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(user_id: current_user.id, **recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe created successfully'
+      redirect_to recipes_path
+    else
+      flash[:alert] = 'Recipe creation failed'
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    # redirect_to recipes_path
+    redirect_to request.referrer
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :prepation_time, :cooking_time, :public)
   end
 end
