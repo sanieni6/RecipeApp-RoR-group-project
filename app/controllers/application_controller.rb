788c54184e9dd4095b0ba3bd02b13de1025a21c8
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-27 15:44:53 -0500

    Configure cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  include LoadAndAuthorizeResource
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 
   protected
 
