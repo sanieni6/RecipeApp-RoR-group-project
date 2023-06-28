2821288838dd3a83df0d52379760c78029f678d0
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-28 17:13:55 -0500

    Create view for foods

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,8 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe, foreign_key: :recipe_id
   belongs_to :food, foreign_key: :food_id
+
+  def calculate_value_by_quantity
+    quantity * food.price
+  end
 end
