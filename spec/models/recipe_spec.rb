4f20669eace2a6a8e6d806cc63cc373c5a4ebb6a
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-29 17:25:57 -0500

    Add test for models

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-    let(:user) { User.create(name: 'luis', email: 'example@email.com', password: 'password')}
-    let(:recipe) { Recipe.create(name: 'recipe', description:'a delicious recipe', user_id: user.id) }
+  let(:user) { User.create(name: 'luis', email: 'example@email.com', password: 'password') }
+  let(:recipe) { Recipe.create(name: 'recipe', description: 'a delicious recipe', user_id: user.id) }
 
-
-    context 'validations' do
-        it('should be valid') do
-            expect(recipe).to be_valid
-        end
-
-        it('should not be valid without a name') do
-            recipe.name = nil
-            expect(recipe).to_not be_valid
-        end
-
-        it('should not be valid without a description') do
-            recipe.description = nil
-            expect(recipe).to_not be_valid
-        end
-
+  context 'validations' do
+    it('should be valid') do
+      expect(recipe).to be_valid
     end
 
+    it('should not be valid without a name') do
+      recipe.name = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it('should not be valid without a description') do
+      recipe.description = nil
+      expect(recipe).to_not be_valid
+    end
+  end
 end
