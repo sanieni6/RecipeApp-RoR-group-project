5b2055f26473aae710b3ab3a9b867a3759697af9
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-30 15:11:31 -0500

    Refactor methods

--- app/controllers/generalshoppinglist_controller.rb
+++ app/controllers/generalshoppinglist_controller.rb
@@ -4,35 +4,8 @@ class GeneralshoppinglistController < ApplicationController
     @recipes = current_user.recipes.includes(:foods)
     @general_foods = current_user.foods
 
-    @missing_foods = calculate_missing_foods
+    @missing_foods = @all_recipes.flat_map { |recipe| recipe.missing_foods(@general_foods) }
     @total_count = @missing_foods.length
-    @total_price = calculate_total_price(@missing_foods)
-  end
-
-  private
-
-  def calculate_missing_foods
-    missing_foods = []
-
-    @all_recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        food = recipe_food.food
-        general_food = @general_foods.find_by(name: food.name)
-
-        next unless general_food.nil? || general_food.quantity < recipe_food.quantity
-
-        missing_foods << {
-          name: food.name,
-          quantity: recipe_food.quantity - (general_food&.quantity || 0),
-          price: food.price
-        }
-      end
-    end
-
-    missing_foods
-  end
-
-  def calculate_total_price(foods)
-    foods.sum { |food| food[:price] * food[:quantity] }
+    @total_price = Recipe.new.total_price(@missing_foods)
   end
 end
